534b3c354ca4191ff3925fbf56a9b9740a8332b2
TanveerNS <44171601+TanveerNS@users.noreply.github.com>
2022-01-29 18:03:42 +0530

    Add files via upload

---
+++ A_SwingExample3.java
@@ -0,0 +1,15 @@
+import javax.swing.*;  
+public class A_SwingExample3 extends JFrame{//inheriting JFrame  
+JFrame f;  
+A_SwingExample3(){  
+JButton b=new JButton("click");//create button  
+b.setBounds(130,100,100, 40);  
+          
+add(b);//adding button on frame  
+setSize(400,500);  
+setLayout(null);  
+setVisible(true);  
+}  
+public static void main(String[] args) {  
+new A_SwingExample3();  
+}}  
